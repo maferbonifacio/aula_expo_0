6654b0a2ae8939a252cf5b76f769ba006d791746
maferbonifacio <160647200+maferbonifacio@users.noreply.github.com>
2024-03-22 09:48:13 -0300

    aula 22-03

--- App.js
+++ App.js
@@ -18,8 +18,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'orange',
+    backgroundColor: 'violet',
     alignItems: 'center',
     justifyContent: 'center',
+    padding: 18,
   },
 });
